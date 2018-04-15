4831f7e561537bb23461963bcadf1be5bb09b4bf
Sean Murphy <murphysean84@gmail.com>
2018-04-15 14:21:43 -0600

    Bugfix expectlast3 works now

--- filter.go
+++ filter.go
@@ -16,12 +16,12 @@ func (g Game) Filter(ctx context.Context) Game {
 	}
 	//Filter the draw and dscard pile
 	if g.PreviousPresidentID == playerID && g.Facist == 3 && len(g.Players) < 7 && g.PreviousEnactedPolicy == PolicyFacist {
-		g.Draw = maskedPolicies(len(g.Draw), true)
+		g.Draw = maskedPolicies(g.Draw, true)
 	} else {
-		g.Draw = maskedPolicies(len(g.Draw), false)
+		g.Draw = maskedPolicies(g.Draw, false)
 	}
 
-	g.Discard = maskedPolicies(len(g.Discard), false)
+	g.Discard = maskedPolicies(g.Discard, false)
 	//Filter the player roles
 	nps := []Player{}
 	for _, p := range g.Players {
@@ -63,18 +63,22 @@ func (g Game) Filter(ctx context.Context) Game {
 	}
 	//Filter the round policies
 	if me.ID != g.Round.PresidentID && me.ID != g.Round.ChancellorID {
-		g.Round.Policies = maskedPolicies(len(g.Round.Policies), false)
+		g.Round.Policies = maskedPolicies(g.Round.Policies, false)
 	} else if me.ID == g.Round.ChancellorID && len(g.Round.Policies) > 2 {
-		g.Round.Policies = maskedPolicies(len(g.Round.Policies), false)
+		g.Round.Policies = maskedPolicies(g.Round.Policies, false)
 	}
 
 	return g
 }
 
-func maskedPolicies(len int, exceptlast3 bool) []string {
-	ret := make([]string, len)
-	for i := 0; i < len; i++ {
+func maskedPolicies(policies []string, exceptlast3 bool) []string {
+	ret := make([]string, len(policies))
+	for i := 0; i < len(policies); i++ {
 		ret[i] = PolicyMasked
+		if exceptlast3 && i > len(policies)-3 {
+			ret[i] = policies[i]
+
+		}
 	}
 	return ret
 }
